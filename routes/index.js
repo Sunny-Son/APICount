48494417ccca73e11bfb8d83e11c30e5428f81b5
Insun Son <ison@ison-ltm1.internal.salesforce.com>
2019-06-13 16:38:19 +0900

    commit2

--- routes/index.js
+++ routes/index.js
@@ -55,7 +55,7 @@ router.get('/apicount', function(req, res, next) {
 
   org.getLimits()
   .then(function(results){
-    //      res.render('index', { records: results.records });
+    ///      res.render('index', { records: results.records });
     console.log(results);
     //    res.status(200).json(results.body);
     //    res.status(200).send(converTable_ProductList(req.protocol + '://' + req.get('host') + '/product',results.records));
